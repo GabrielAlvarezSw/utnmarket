91a7fb2087e12ef11cb4ff078d5471474437d321
anto <antocanessa.0694@gmail.com>
2019-08-09 22:14:37 -0300

    Se añadieron metodos de verificacion en la clase Client y se anañadieron los test en la clase ClientTest

--- src/utnmarket/src/models/Client.java
+++ src/utnmarket/src/models/Client.java
@@ -3,17 +3,46 @@
  */
 package utnmarket.src.models;
 
+import exceptions.IllegalTransactionException;
+import java.util.Date;
+
 /**
  *
  * @author Gabriel
  */
 public class Client {
+
     public Cart cart;
     public Card card;
     public String name;
     public String email;
     public String password;
-    
-    
-    
+
+    //Constructor
+    public Client(Cart cart, Card card, String name, String email, String password) {
+        this.cart = cart;
+        this.card = card;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public boolean VerificarUserAndPass(String nombre, String email, String contraseña) throws IllegalTransactionException {
+
+        if (nombre == name && contraseña == password || email == this.email && contraseña == password) {
+            return true;
+        } else {
+            throw new IllegalTransactionException("Usuario inexistente");
+
+        }
+    }
+
+    public  boolean VerificarCamposVacios(String nombre, String email, String contraseña) throws IllegalTransactionException {
+        if (nombre.length() == 0 || email.length() == 0 || contraseña.length() == 0) {
+            throw new IllegalTransactionException("Hay campos vacíos");
+        } else {
+            return true;
+
+        }
+    }
 }
--- test/utnmarket/src/models/ClientTest.java
+++ test/utnmarket/src/models/ClientTest.java
@@ -5,6 +5,8 @@
  */
 package utnmarket.src.models;
 
+import exceptions.IllegalTransactionException;
+import java.util.Date;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -17,30 +19,81 @@ import static org.junit.Assert.*;
  * @author Gabriel
  */
 public class ClientTest {
-    
+
     public ClientTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
 
-    @Test
-    public void testSomeMethod() {
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    /**
+     * Test of VerificarUserAndPass method, of class Client.
+     */
+    @Test //Verificar usuario y contraseña que devuelve True cuando hay coincidencia
+    public void testVerificarUserAndPass() throws Exception {
+        System.out.println("VerificarUserAndPass");
+        String nombre = "";
+        String email = "";
+        String contraseña = "";
+        Card card = new Card("card", "123", new Date());
+        Cart cart = new Cart();
+        Client cliente = new Client(cart, card, nombre, email, contraseña);
+        boolean result = cliente.VerificarUserAndPass(nombre, email, contraseña);
+        assertTrue(result);
     }
-    
+
+    @Test(expected = IllegalTransactionException.class) // Vericar que es test lanzo una excepcion cuando se le paso algo incorrecto
+    public void testVerificarUserAndPassExcepcion() throws IllegalTransactionException {
+        System.out.println("VerificarUserAndPassConException");
+        String nombre = "";
+        String email = "";
+        String contraseña = "";
+        Card card = new Card("card", "123", new Date());
+        Cart cart = new Cart();
+        Client cliente = new Client(cart, card, nombre, email, contraseña);
+        boolean result = cliente.VerificarUserAndPass(nombre, "em", "cont");
+    }
+
+    /**
+     * Test of VerificarCamposVacios method, of class Client.
+     */
+    @Test // Verificar lance true cuando no hayan campos vacios 
+    public void testVerificarCamposVacios() throws Exception {
+        System.out.println("VerificarCamposVacios");
+        String nombre = "algo";
+        String email = "algo";
+        String contraseña = "algo";
+        Card card = new Card("card", "123", new Date());
+        Cart cart = new Cart();
+        Client client = new Client(cart, card, nombre, email, contraseña);
+        boolean result = client.VerificarCamposVacios(nombre, email, contraseña);
+        assertTrue(result);
+     
+    }
+
+    @Test (expected = IllegalTransactionException.class) // Verificar que lance una excepcion cuando hayan campos vacíos
+    public void testVerificarCamposVaciosException()throws IllegalTransactionException {
+        System.err.println("VerificarCamposVaciosConExcepcion");
+        String nombre = "";
+        String email = "";
+        String contraseña = "";
+        Card card = new Card("card", "123", new Date());
+        Cart cart = new Cart();
+        Client cliente = new Client(cart, card, nombre, email, contraseña);
+        boolean result = cliente.VerificarCamposVacios(nombre, email, contraseña);
+    }
+
 }
